4f3f7f98b0f89a550a2b74586557ca6b02af049e
Brian Lu <brianzimolu@gmail.com>
2024-03-06 18:29:07 +1100

    Assignment 1 Iteration 2

--- app/src/main/java/com/example/fit2081assignment1/Login.java
+++ app/src/main/java/com/example/fit2081assignment1/Login.java
@@ -9,13 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Signup extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_login);
     }
 
 }
