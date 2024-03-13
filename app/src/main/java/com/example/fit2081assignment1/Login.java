744bcbcae210c50c9c9f1570fc9f9e34aed459d1
Brian Lu <brianzimolu@gmail.com>
2024-03-13 19:31:53 +1100

    Assignment 1 Iteration 6

--- app/src/main/java/com/example/fit2081assignment1/Login.java
+++ app/src/main/java/com/example/fit2081assignment1/Login.java
@@ -1,7 +1,11 @@
 package com.example.fit2081assignment1;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +14,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    EditText tvLoginUsername;
+    EditText tvLoginPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        tvLoginUsername = findViewById(R.id.editTextLoginUsername);
+        tvLoginPassword = findViewById(R.id.editTextLoginPassword);
     }
+
+    public void onButtonLogin(View view){
+        SharedPreferences saveUserInformationToSharedPreference = getSharedPreferences("USER_INFORMATION", MODE_PRIVATE);
+
+        String stringLogiuserName = tvLoginUsername.getText().toString();
+        String stringLoginPassword = tvLoginPassword.getText().toString();
+        String restoredUsername = saveUserInformationToSharedPreference.getString("USER_USERNAME", null);
+        String restoredPassword = saveUserInformationToSharedPreference.getString("USER_PASSWORD", null);
+
+        if(stringLogiuserName.equals(restoredUsername) && stringLoginPassword.equals(restoredPassword)){
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
 
 }
--- app/src/main/java/com/example/fit2081assignment1/Signup.java
+++ app/src/main/java/com/example/fit2081assignment1/Signup.java
@@ -1,21 +1,67 @@
 package com.example.fit2081assignment1;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Signup extends AppCompatActivity {
+
+    EditText tvUsername;
+    EditText tvPassword;
+    EditText tvConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+
+        tvUsername = findViewById(R.id.editTextUsername);
+        tvPassword = findViewById(R.id.editTextPassword);
+        tvConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+    }
+
+    public void onButtonRegister(View view){
+        String usernameString = tvUsername.getText().toString();
+        String passwordString = tvPassword.getText().toString();
+        String confirmPasswordString = tvConfirmPassword.getText().toString();
+
+        if (TextUtils.isEmpty(usernameString) || TextUtils.isEmpty(passwordString) || TextUtils.isEmpty(confirmPasswordString)){
+            Toast.makeText(this, "Invalid username or password",Toast.LENGTH_SHORT).show();
+        }
+
+        else if(passwordString.equals(confirmPasswordString)){
+            Toast.makeText(this, "User successfully registered", Toast.LENGTH_SHORT).show();
+            saveUserInformationToSharedPreference(usernameString, passwordString);
+
+            Intent loginPage = new Intent(this, Login.class);
+            loginPage.putExtra("Username", usernameString);
+            loginPage.putExtra("Password", passwordString);
+            startActivity(loginPage);
+        }
+
+        else{
+            Toast.makeText(this,"Passwords are different", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
+
+    private void saveUserInformationToSharedPreference(String usernameValue, String passwordValue) {
+        SharedPreferences saveUserInformationToSharedPreference = getSharedPreferences("USER_INFORMATION", MODE_PRIVATE);
+        SharedPreferences.Editor editor = saveUserInformationToSharedPreference.edit();
+        editor.putString("USER_USERNAME", usernameValue);
+        editor.putString("USER_PASSWORD", passwordValue);
+        editor.apply();
     }
 
 }
