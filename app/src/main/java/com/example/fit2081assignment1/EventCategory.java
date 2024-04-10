ecbdc1d3bf3c05a77dea6166d1ec7e05a0e56ce8
Brian Lu <brianzimolu@gmail.com>
2024-04-11 02:06:33 +1000

    Assignment 2 Iteration 10

--- app/src/main/java/com/example/fit2081assignment1/EventCategory.java
+++ app/src/main/java/com/example/fit2081assignment1/EventCategory.java
@@ -5,5 +5,34 @@ public class EventCategory {
         private String eventName;
         private String categoryID;
         private int tickets;
+
+        public EventCategory(String eventID, String eventName, String categoryID, int tickets, boolean ticketsAvailable) {
+                this.eventID = eventID;
+                this.eventName = eventName;
+                this.categoryID = categoryID;
+                this.tickets = tickets;
+                this.ticketsAvailable = ticketsAvailable;
+        }
+
+        public String getEventID() {
+                return eventID;
+        }
+
+        public String getEventName() {
+                return eventName;
+        }
+
+        public String getCategoryID() {
+                return categoryID;
+        }
+
+        public int getTickets() {
+                return tickets;
+        }
+
+        public boolean isTicketsAvailable() {
+                return ticketsAvailable;
+        }
+
         private boolean ticketsAvailable;
     }
